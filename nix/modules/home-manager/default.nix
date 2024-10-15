274f794ec2670d7bc0b5e9f1a0b138da7f89f96e
Jonathan Guevara <memooguevara@gmail.com>
2024-10-15 17:44:51 -0500

    feat: add some packages httpie, wl-clipboard
    - Change curl by httpie
    - Change xsel by wl-clipboard
    - Update flake.lock

--- nix/modules/home-manager/default.nix
+++ nix/modules/home-manager/default.nix
@@ -7,12 +7,12 @@
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "JetBrainsMono" "IosevkaTerm" ]; })
-    curl
+    httpie # Better curl
     eza # Better ls
     fd # Better find
     neofetch
     nixd # Nix LSP
     alejandra # Nix formatter
-    xsel
+    wl-clipboard
   ];
 }
