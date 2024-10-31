0230d317ed3d933500e0767d79ffecfce9af0865
Jonathan Guevara <memooguevara@gmail.com>
2024-10-31 12:13:16 -0500

    refactor: change import to general.import in alacritty

--- nix/modules/home-manager/apps/terminals/alacritty.nix
+++ nix/modules/home-manager/apps/terminals/alacritty.nix
@@ -1,13 +1,11 @@
-{ inputs, lib, config, pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.alacritty = {
     enable = true;
     settings = {
-      import = with pkgs; [ alacritty-theme.tokyo-night ];
+      general.import = with pkgs; [alacritty-theme.tokyo-night];
       font = {
         normal.family = "IosevkaTerm NFM";
-	size = 18;
+        size = 18;
       };
       window.opacity = 0.95;
     };
