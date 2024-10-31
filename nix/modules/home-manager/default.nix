d453837d3d0010b6d625a4759d437ec89c899104
Jonathan Guevara <memooguevara@gmail.com>
2024-10-31 12:11:37 -0500

    feat: remove neofetch and add fastfetch and add curl

--- nix/modules/home-manager/default.nix
+++ nix/modules/home-manager/default.nix
@@ -1,16 +1,19 @@
-{ inputs, lib, config, pkgs, ... }:
-
 {
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     ./apps
   ];
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" "IosevkaTerm" ]; })
+    (nerdfonts.override {fonts = ["JetBrainsMono" "IosevkaTerm"];})
+    curl
     httpie # Better curl
     eza # Better ls
     fd # Better find
-    neofetch
+    fastfetch
     wl-clipboard
   ];
 }
